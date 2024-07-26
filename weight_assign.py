ef62df9e4790e17c922fc47d7f0b40e2b4e3a2b3
Devika <115264420+DEVIKA201@users.noreply.github.com>
2024-07-26 11:57:24 +0530

    Update weight_assign.py

--- weight_assign.py
+++ weight_assign.py
@@ -1,22 +1,29 @@
 import mysql.connector
 import pandas as pd
 
-def fetch_data(config, query):
-    try:
-        link = mysql.connector.connect(**config)
-        cursor = link.cursor(dictionary=True)
-        cursor.execute(query)
-        data = cursor.fetchall()
-        cursor.close()
-        link.close()
-        return data
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return []
+def fetch_data(config, query, retries=3, dealy=5):
+    attempt = 0
+    while attempt<retries:
+        try:
+            link = mysql.connector.connect(**config)
+            cursor = link.cursor(dictionary=True)
+            cursor.execute(query)
+            data = cursor.fetchall()
+            cursor.close()
+            link.close()
+            return data
+        except mysql.connector.Error:
+            attempt+=1
+            if attempt>=retries:
+                return []
 
 def assign_weights_from_table(config, table_name):
     data = fetch_data(config, f"SELECT id, conv_rate FROM {table_name}")
+    if not data:
+        return{}
     total_weight = sum(float(row['conv_rate']) for row in data)
+    if total_weight ==0:
+        return {row['id']:0 for row in data}
     weights = {row['id']: float(row['conv_rate']) / total_weight for row in data}
     return weights
 
@@ -30,13 +37,12 @@ def assign_weights_from_database(config):
 def get_lead_data(config):
     query = """
         SELECT ut.id as enquiry_id, u.patient_name, s.id as source_id, s.name as source, t.id as treatment_id, t.treatment_name as treatment, 
-        l.id as location_id, l.name as location, uts.name as transaction_status, tm.name as bd
+        l.id as location_id, l.name as location,ut.user_trxn_status_id, tm.name as bd,ut.created_date
         FROM user_transaction ut
         LEFT JOIN users u ON ut.user_id = u.id
         LEFT JOIN source s ON s.id = ut.source_id
         LEFT JOIN treatment t ON t.id = u.treatment_id
         LEFT JOIN location l ON l.id = u.location_id
-        LEFT JOIN user_trxn_status uts ON uts.id = ut.user_trxn_status_id
         LEFT JOIN team_mapppings tm ON tm.code = ut.leads_assign_to
     """
     return fetch_data(config, query)
@@ -51,41 +57,51 @@ def preprocess_data(leads, weights):
                                     lead['location_weight'])
     return leads
 
+def update_user_txn_status(leads, config):
+    user_txn_status_map = fetch_data(config, "SELECT id, name FROM user_trxn_status")
+    if user_txn_status_map:
+        status_map = {status['id']:status['name'] for status in user_txn_status_map}
+        for lead in leads:
+            lead['transaction_status'] = status_map.get(lead['user_trxn_status_id'],'Unknown')
+        return leads
+    
 config = {
-    'user': 'root',
-    'password': 'root',
-    'host': 'localhost',
-    'port': 8889,
-    'database': 'bd_data',
-    'raise_on_warnings': True,
+    'user': 'username',
+    'password': 'password',
+    'host': 'hostname',
+    'port': portnumber,
+    'database': 'dbname',
 }
 
 weights = assign_weights_from_database(config)
 leads = get_lead_data(config)
-leads = preprocess_data(leads, weights)
 
-df = pd.DataFrame(leads)
+if leads:
+    leads = preprocess_data(leads,weights)
+    leads = update_user_txn_status(leads,config)
+    
+    df = pd.DataFrame(leads)
+    pd.options.display.float_format = '{:.4f}'.format
 
-# Display the DataFrame with all float values up to 4 decimal places
-pd.options.display.float_format = '{:.4f}'.format
+    if 'total_importance' in df.columns:
+        # define thresholds
+        high_threshold = 0.25
+        low_threshold = 0.095
+        
+        def categorize_importance(importance):
+            if importance >= high_threshold:
+                return 'Critical'
+            elif low_threshold < importance < high_threshold:
+                return 'Essential'
+            else:
+                return 'Routine'
 
-# define thresholds
-high_threshold = 0.25
-low_threshold = 0.095
-
-def categorize_importance(importance):
-    if importance >= high_threshold:
-        return 'Critical'
-    elif low_threshold < importance < high_threshold:
-        return 'Essential'
-    else:
-        return 'Routine'
-
-df['priority'] = df['total_importance'].apply(categorize_importance)
+        df['priority'] = df['total_importance'].apply(categorize_importance)
 # Ordinal encoding in increasing order of Routine, Essential, Critical
-priority_mapping = {'Routine': 0, 'Essential': 1, 'Critical': 2}
-df['priority_encoded'] = df['priority'].map(priority_mapping)
+        priority_mapping = {'Routine': 0, 'Essential': 1, 'Critical': 2}
+        df['priority_encoded'] = df['priority'].map(priority_mapping)
 
-df = df[['enquiry_id', 'patient_name', 'bd', 'source', 'treatment', 'location', 'total_importance', 'priority']]
-#print(df)
-#print(df[['total_importance','priority']]) 
+        df = df[['enquiry_id', 'created_date', 'patient_name', 'transaction_status', 'bd', 'source', 'treatment', 'location', 'priority']]
+else:
+    pass
+
